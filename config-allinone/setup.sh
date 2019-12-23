ca8bad3ced7f5c1483328e2418736df299e0d4de
jnunyez <jonunyez@gmail.com>
2019-12-23 22:52:41 +0100

    second commit fixing allinone

--- config-allinone/setup.sh
+++ config-allinone/setup.sh
@@ -5,6 +5,11 @@ if ! grep "ogstun" /proc/net/dev > /dev/null; then
 fi
 ip addr del 45.45.0.1/16 dev ogstun 2> /dev/null
 ip addr add 45.45.0.1/16 dev ogstun
-ip addr del cafe::1/64 dev ogstun 2> /dev/null
-ip addr add cafe::1/64 dev ogstun
+#ip addr del cafe::1/64 dev ogstun 2> /dev/null
+#ip addr add cafe::1/64 dev ogstun
+
 ip link set ogstun up
+sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -I INPUT -i ogstun -j ACCEPT
+
