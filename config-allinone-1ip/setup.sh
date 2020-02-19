23dd52348ce5f9ad7ac8c3a3a6f38e578ae15d25
jnunyez <jonunyez@gmail.com>
2020-02-19 22:21:38 +0100

    pgw tuntap setting

--- config-allinone-1ip/setup.sh
+++ config-allinone-1ip/setup.sh
@@ -8,7 +8,10 @@ ip addr add 45.45.0.1/16 dev ogstun
 #ip addr del cafe::1/64 dev ogstun 2> /dev/null
 #ip addr add cafe::1/64 dev ogstun
 
-ip link set ogstun up
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -I INPUT -i ogstun -j ACCEPT
+ip r del default via 192.168.50.1
+ip r add default via 172.30.0.1
 
+
+ip link set ogstun up
+iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+iptables -I INPUT -i ogstun -j ACCEPT
