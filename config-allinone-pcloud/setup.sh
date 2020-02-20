4e31173c07bcb73b568097419d5e81e8fea976ff
Jose Nuñez <jose.nunezmartinez@telefonica.com>
2020-02-20 15:01:12 +0100

    open5gs cfg public cloud hosted

---
+++ config-allinone-pcloud/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if ! grep "ogstun" /proc/net/dev > /dev/null; then
+    ip tuntap add name ogstun mode tun
+fi
+ip addr del 45.45.0.1/16 dev ogstun 2> /dev/null
+ip addr add 45.45.0.1/16 dev ogstun
+#ip addr del cafe::1/64 dev ogstun 2> /dev/null
+#ip addr add cafe::1/64 dev ogstun
+
+ip link set ogstun up
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -I INPUT -i ogstun -j ACCEPT
