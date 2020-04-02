fa661c8cfcc2a8d8e90418d2c27f878606538437
Jose Nuñez <jose.nunezmartinez@telefonica.com>
2020-04-02 16:21:22 +0200

    k8s cfg cpf

---
+++ dissag-cfg/k8s_cfg_cpf/config-diss-cpf-kube/provisioning/db/run_db.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -n "${DB_HOST}" ]; then
+  echo "Database variable found"
+else
+  echo "Database variable not found, using default one"
+  DB_HOST=mongodb
+fi
+
+echo "Will use ${DB_HOST}"
+
+if [ -n "${MONGODB_STARTUP_TIME}" ]; then
+  echo "Mongodb startup wait found"
+else
+  echo "Mongodb startup wait variable not found, using default one"
+  MONGODB_STARTUP_TIME=5
+fi
+echo "Waiting for " ${MONGODB_STARTUP_TIME} "s for mongodb to be ready..."
+sleep ${MONGODB_STARTUP_TIME}
+
+mongoimport --host ${DB_HOST} --db open5gs --collection subscribers --file /tmp/imsi1.json --type json  --jsonArray
+sleep infinity
